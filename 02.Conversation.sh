e1aca3a916e3a944736ab5fb6e17186d9c6af942
Kishore7711 <kishorebalusu7@gmail.com>
2025-09-21 16:18:34 +0530

    some massage

---
+++ 02.Conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Murthy::: Hi, How are you Kiran"
+echo "Kiran::: Hi Murthy, I am fine. How about you"
+echo "Murthy::: I am also fine. What are you doing these days?"
+echo "Kiran::: I am learning DevOps. What about you"
+echo "Murty::: I am also learining Shell Scripting Kiran"
