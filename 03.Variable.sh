e56e936d91a8c79d371990f63de46c2b8e7d8f2a
Kishore7711 <kishorebalusu7@gmail.com>
2025-09-21 16:40:10 +0530

    some massage

---
+++ 03.Variable.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1="Murthy"
+PERSON2="Kiran"
+
+echo "$PERSON1::: Hi, How are you $PERSON2"
+echo "$PERSON2::: Hi $PERSON1, I am fine. How about you"
+echo "$PERSON1::: I am also fine. What are you doing these days? $PERSON2"
+echo "$PERSON2::: I am learning DevOps. What about you $PERSON1"
+echo "$PERSON1::: I am also learining Shell Scripting $PERSON2"
