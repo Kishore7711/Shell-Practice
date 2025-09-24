2304b381bf46a5a64ae9286f9f891b2b193e63cb
Kishore7711 <kishorebalusu7@gmail.com>
2025-09-24 23:27:39 +0530

    variable commit

---
+++ 04.Variable.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: Welcome to Shell Scripting online session $PERSON2"
+echo "$PERSON2:: Thank you $PERSON1 for the session"
+echo "$PERSON1:: You are welcome $PERSON2"
+echo "$PERSON2:: I have a question $PERSON1"
+
