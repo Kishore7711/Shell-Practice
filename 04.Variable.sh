9051ec1aa8566d6865348a67492dee5b27123fdf
Kishore7711 <kishorebalusu7@gmail.com>
2025-09-24 23:47:39 +0530

    last commit

--- 04.Variable.sh
+++ 04.Variable.sh
@@ -3,6 +3,8 @@
 PERSON1=$1
 PERSON2=$2
 
+###### this is Arguments ########
+
 echo "$PERSON1:: Welcome to Shell Scripting online session $PERSON2"
 echo "$PERSON2:: Thank you $PERSON1 for the session"
 echo "$PERSON1:: You are welcome $PERSON2"
