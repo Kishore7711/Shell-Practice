1de7c868ea70185f648150034ba14b7a88e0093a
Kishore7711 <kishorebalusu7@gmail.com>
2025-09-27 00:24:16 +0530

    Dynamic

---
+++ 07.Dynamic_Variable.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+######## how do you run a command inside shell script and take the ourput ##########3
+
+####### script start time
+###### script end time
+##### executed in end time - start time
+#### VAR_NAME=$(command)
+
+DATE=$(date)
+
+echo "Time Stamp is : $DATE"
+
