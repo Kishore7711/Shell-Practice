1435e9cafa28b11ee6a57557b1ea843116a5f5b0
Kishore7711 <kishorebalusu7@gmail.com>
2025-09-27 01:00:25 +0530

    all commits

--- 08.Special_Variable.sh
+++ 08.Special_Variable.sh
@@ -2,3 +2,10 @@
 
 echo "All Variable passed to the script: $@"
 echo "All Variable passed to the script: $*"
+echo "Script name is : $0"
+echo "Current directory is : $PWD"
+echo "who is running this script : $USER"
+echo "Home directory of user is : $HOME"
+echo "PID of this script is : $$"
+sleep 50
+echo "PID of last command in background is : $!"
