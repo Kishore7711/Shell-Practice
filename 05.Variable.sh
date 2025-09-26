6c2748d75436b78f26e638f46790d7c76b66fd3f
Kishore7711 <kishorebalusu7@gmail.com>
2025-09-26 23:58:55 +0530

    ENV_Commit

--- 05.Variable.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "please enter your PIN Number"
-read -s PIN_NUMBER ## here PIN_NUMBER is a variable
-
-echo "PIN entered is : $PIN_NUMBER"
---
+++ 06.ENV_Variable.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+############# ENV Variable -- export #################
+
+###### do you want to changes permanent ENV Variable go to ~ .bashrc ##########
+
+######## export COURSE="DevOps with AWS"
+
+########### echo $COURSE
+
+echo "Course name is : $COURSE"
+
+
