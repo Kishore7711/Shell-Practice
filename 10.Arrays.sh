7c7cb9226847f77157f4047497240894803deb59
Kishore7711 <kishorebalusu7@gmail.com>
2025-09-27 19:50:49 +0530

    Array changes

--- 10.Arrays.sh
+++ 10.Arrays.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-##########  list of name is stored in array ###########
+##########  list of name is stored in one array ###########
+
+#### size = 4, Max Index = 3
 
 Names=( "Anil" "Sunil" "Kiran" "Ravi" )
 
 echo "first name is : ${Names[0]}"
+
+Leaders=( "modi" "rahul" "yogi" "mamta" )
+
+echo "All Leaders are : ${Leaders[@]}"
+echo "first Leader is : ${Leaders[0]}"
+echo "Last Leader is :${Leaders[10]}"  ### out of bound index
+
+
 
