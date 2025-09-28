23294100bc58a7ab4f32811b4583c1577df79292
Kishore7711 <kishorebalusu7@gmail.com>
2025-09-28 22:38:06 +0530

    installation commits done

--- 17.Colours_with_Install.sh
+++ 17.Colours_with_Install.sh
@@ -8,17 +8,17 @@ N="\e[0m"   ### N for Normal
 USERID=$(id -u)
 
 if [ $USERID -ne 0 ] ; then
-    echo -e "$R ERROR:: Please run this script with ROOT Privilages $N
+    echo -e "$R ERROR:: Please run this script with ROOT Privilages $N"
     exit 1
 fi
 
 VALIDATE(){
-    if [ $1 -ne 0 ]; then
+    if [ $1 -ne 0 ] ; then
         echo -e "$R ERROR:: $2 Installation Failed $N"
     else
-        echo -e $G SUCCESS:: $2 Installation Successful $N
+        echo -e "$G SUCCESS:: $2 Installation Successful $N"
     fi
-}
+          }
 
 dnf list installed mysql
 ####  install if it is not found
