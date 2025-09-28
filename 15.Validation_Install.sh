bcb8990d34d1d1e5d03383e3ac579e8032026345
Kishore7711 <kishorebalusu7@gmail.com>
2025-09-28 16:56:00 +0530

    commited

--- 15.Validation_Install.sh
+++ 15.Validation_Install.sh
@@ -2,27 +2,28 @@
 
 ######### scripting /programming --- 1) Keep less lines more performance, 2) Readability, 3) DRY -- Do not repeat yourself -- TO reuse the code #########
 
-####### Functions ------- takes some input and give some output
+####### Functions ------- takes some input and give some outpu
 
 USERID=$(id -u)
 
-if [ $USERID -ne 0 ] ; then
-    echo "ERROR:: Please run this Script with ROOT Prilivileges"
-    exit 1 ## failure is other than 0
-if
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run the script with ROOT Privilages "
+    exit 1
+fi
 
-VALIDATE() {  ### Function receive inputs through $1, $2, $3 ... args just like script args 
-    if [ $1 -ne 0 ] ; then
-        echo "ERROR::  Installation $2 is Failure"
-    else
-        echo "SUCCESS:: Installation $2  is Successfull"
-    fi
+VALIDATE (){  #### fuctions receive  input through parameters ($1,$2 -- arguments) --- like Shell script arguments  
+    if [ $1 -ne 0 ]; then
+        echo "ERROR:: $2 Installation Failed"
+    else 
+        echo "SUCCESS:: $2 Installation Successful"
+    fi 
            }
-dnf install mysql -y
-VALIDATE $? "Mysql"
 
-dnf install nginx -y
-VALIDATE $? "Nginx"
+ dnf install mysql -y
+ VALIDATE $? "Mysql"
 
-dnf install python3 -y
-VALIDATE $? "Python3"
+ dnf install nginx -y
+ VALIDATE $? "nginx"
+
+ dnf install python3
+ VALIDATE $? "Python3"
