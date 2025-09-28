ab790c5d68a0d0e2946386c8ee0657439c7b4e49
Kishore7711 <kishorebalusu7@gmail.com>
2025-09-28 11:01:30 +0530

    changes commit done

--- 11.if .. else.Conditions.sh
+++ 11.if .. else.Conditions.sh
@@ -32,6 +32,8 @@ NUMBER=$1
 
 if [ $NUMBER -lt 20 ]; then
     echo "Given number $NUMBER is less than 20"
+elif [ $NUMBER -eq 20 ]; then
+    echo "Given number #NUMBER is qual to 20"
 else
     echo "Given number $NUMBER is greater than or equal to 20"
 fi
