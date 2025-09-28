954c20341a95a324f13027157ed28b151120a9b5
Kishore7711 <kishorebalusu7@gmail.com>
2025-09-28 17:45:23 +0530

    colours commit

---
+++ 16.Colours.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+######### [ ec2-user@ip-172-31-30-194 ~/Shell-Practice ]$ echo -e "\e[31m hill hello welcome to DevOPs with AWS"
+ ##### hill hello welcome to DevOPs with AWS   ---- Red Colour
+
+######## 34.229.143.18 | 172.31.30.194 | t3.micro | https://github.com/Kishore7711/Shell-Practice.git
+######## [ ec2-user@ip-172-31-30-194 ~/Shell-Practice ]$ echo -e "\e[32m im coming from Andhra Pradesh"
+ ####### i'm coming from Andhra Pradesh ---- Green Colour
+
+######## 34.229.143.18 | 172.31.30.194 | t3.micro | https://github.com/Kishore7711/Shell-Practice.git
+######## [ ec2-user@ip-172-31-30-194 ~/Shell-Practice ]$ echo -e "\e[33m this is kishore"
+ ####### this is kishore ---- Yellow Colour
+
+######## 34.229.143.18 | 172.31.30.194 | t3.micro | https://github.com/Kishore7711/Shell-Practice.git
+######## [ ec2-user@ip-172-31-30-194 ~/Shell-Practice ]$ echo -e "\e[34m today is BIRTHDAY"
+ ####### today is BIRTHDAY ---- Blue Colour
+
+
+######## 34.229.143.18 | 172.31.30.194 | t3.micro | https://github.com/Kishore7711/Shell-Practice.git
+######## [ ec2-user@ip-172-31-30-194 ~/Shell-Practice ]$ echo -e "\e[35m my coutry name INDIA"
+ ####### my coutry name INDIA ---- Magenta Colour
+
+######## 34.229.143.18 | 172.31.30.194 | t3.micro | https://github.com/Kishore7711/Shell-Practice.git
+######## [ ec2-user@ip-172-31-30-194 ~/Shell-Practice ]$ echo -e "\e[36m My Village Name is ANGARA"
+ ####### My Village Name is ANGARA ---- Cyan Colour
+
+######## 34.229.143.18 | 172.31.30.194 | t3.micro | https://github.com/Kishore7711/Shell-Practice.git
+######## [ ec2-user@ip-172-31-30-194 ~/Shell-Practice ]$ echo -e "\e[37m My Company name is ISG"
+ #######My Company name is ISG ---- White Colour
+
+ R="\e[31m"
+ G=""\e[32m"
+ Y="\e[33m"
+ N="\e[37m"   ### N for Normal
+
+    echo -e "$R" HILL WELCOME TO DEVOPS WITH AWS "$N"
+    echo -e "check this colour"
