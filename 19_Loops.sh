a433f1e4156d67e2c4c8305390f58bb5b6d94d07
Kishore7711 <kishorebalusu7@gmail.com>
2025-10-02 13:06:56 +0530

    all tasks are commited

---
+++ 19_Loops.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+##### for i in {1..30}
+### do 
+    ### echo $i 
+
+### done
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"  #### N for Normal Color
+
+LOGS_FOLDER="/var/log/shell-script"
+SCRIPT_NAME=$(echo "19_Loops.sh" | cut -d "." -f1)
+LOGS_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+echo "Script Exectution Started at : $(date)" | tee -a $LOGS_FILE"
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R ERROR:: $N Please run this script with $B ROOT Prilivages $N" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ] ; then  #### $1 is exit status of the last executed command
+        echo -e "$R ERROR:: $N $2 Installation $R Failed $N" | tee -a $LOGS_FILE
+    else
+        echo -e "$G SUCCESS:: $N $2 Installation $G Successful $N" | tee -a $LOGS_FILE
+    fi
+        }
