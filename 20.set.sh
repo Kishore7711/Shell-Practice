98f17d2281885a65088fecd19e01707debbfbe46
Kishore7711 <kishorebalusu7@gmail.com>
2025-10-04 18:02:39 +0530

    commits

--- 20.set.sh
+++ 20.set.sh
@@ -2,10 +2,16 @@
 
 set -e
 
+## error (){
+##    echo "There is an Error in $LINENO, Command is : $BASH_COMMAND"
+##         }
+
+tranp 'echo "There is an Error in $LINENO, Command is : $BASH_COMMAND"' ERR
+
 echo "hi hello welcome to AWS"
 
 echo "before ERROR"
 
-DKJFFDFIFDFJJ
+DKJFFDFIFDFJJ  #### here shell understand there is an error and signal is ERR
 
 echo "after ERROR"
